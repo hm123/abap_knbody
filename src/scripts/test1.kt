f531a3fd9e214cbafb953c27247c7d639dfc2ee7
hm123 <hugo.martay@gmail.com>
2018-11-14 21:00:21 +0000

    bugs with null outdir

--- src/scripts/test1.kt
+++ src/scripts/test1.kt
@@ -9,7 +9,7 @@ import java.io.File
 
 
 fun main(args:Array<String>){
-
+    println("Version 2018-11-14")
     //Set default parameters.
     var outdir : String? = null
     var n = 1500
@@ -33,6 +33,7 @@ fun main(args:Array<String>){
     var s : State? = null
     val fn = "$outdir/initial.json"
     if (useLastSim) {
+        if (outdir==null) throw Exception("null outdir, but use last sim are incompatible")
         s = fn.loadJson(State::class.java)
     }
     else{
@@ -40,13 +41,14 @@ fun main(args:Array<String>){
         // This is where the initial state is generated if not reused.
         // This actually carries out its own simulations to form the initial planets.
         // These mini-simulations get saved in outdir/planets.
-        s = newSim(n, "$outdir/planets", show)
-        s.saveJson("$outdir/initial.json")
+
+        s = newSim(n, if (outdir==null) null else "$outdir/planets", show)
+        if (outdir!=null) s.saveJson("$outdir/initial.json")
     }
     if (s.p.size!=n) throw Exception("You specified using the last sim, but n is different.")
 
-    // Now we have the main simulation loop, with 30k steps, producing 1000 frames.
-    for(i in 0 until 30000) {
+    // Now we have the main simulation loop, with 100k steps, producing 3333 frames.
+    for(i in 0 until 100000) {
         if ((i%5)==0) println("$i")
         s.timestep(dt, false)
         if (i%30 == 0) s.output(outdir, show)
